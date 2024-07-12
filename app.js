fffa365dda336f102b8784c90fa37051adfd3e61
solzambrano <solzambrano.8@gmail.com>
2024-07-12 16:58:32 -0300

    configuracion ruta home

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 
 const app = express();
-
+app.use(express.static('./public'))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.set('view engine', 'ejs');
-app.set('views', 'src/views');
 
+
+const home=require('./routes/routesHome')
 
 app.listen(3000, () => {
   console.log('listening in http://localhost:3000');
 });
+
+
+//rutas
+
+app.use('/',home)
+
+app.use(((req,res,next)=>{
+    res.status(404).render('not-found')
+}))
---
+++ controllers/homeController.js
@@ -0,0 +1,7 @@
+const homeController={
+    home:(req,res)=>{
+        res.render('home')
+    }
+}
+
+module.exports=homeController
