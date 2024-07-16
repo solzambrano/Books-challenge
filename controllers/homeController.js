71795c253d4a5b6eaaa08978ad951b2496fbd6c9
solzambrano <solzambrano.8@gmail.com>
2024-07-16 12:13:37 -0300

    impleementacion editBook

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -11,10 +11,36 @@ const homeController={
         .catch((err) => console.log(err));
     },
     detailBook:(req,res)=>{
-        db.Book.findByPk(req.params.id)
+        db.Book.findByPk(req.params.id,{
+             include:[{association:'authors'}]
+        })
         .then(book=>{
             res.render('bookDetail',{book})
         })
+        .catch(err=>console.log(err))
+    },
+    updateBook:(req,res)=>{
+        db.Book.findByPk(req.params.id)
+        .then(book=> res.render('editBook',{book}))
+    },
+    processUpdateBook:(req,res)=>{
+        db.Book.update({
+            title:req.body.title,
+            cover:req.body.cover,
+            description:req.body.description
+        },
+        {where:{id:req.params.id}}
+        )
+       .then((book) => {
+        console.log(req.params.id);
+        console.log(book[0]);
+        if (book[0] === 1) {
+         res.redirect('/');
+        } else {
+         res.status(404).send('Book not found');
+         }
+        })
+        .catch(err=>console.log(err))
     }
 }
 
--- routes/routesBook.js
+++ routes/routesBook.js
@@ -5,4 +5,7 @@ const homeController=require('../controllers/homeController')
 router.get('/',homeController.listBook)
 router.get('/books/detail/:id',homeController.detailBook)
 
+router.get('/books/edit/:id',homeController.updateBook)
+router.put('/books/edit/:id',homeController.processUpdateBook)
+
 module.exports=router
