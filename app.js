e1876de357d1a9b889988b2ec0da9bdf8f8abf3e
solzambrano <solzambrano.8@gmail.com>
2024-07-16 12:11:10 -0300

    termina configuracion detail

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const method=require('method-override');
 
 const app = express();
 app.use(express.static('./public'))
+//configuracion put y delete
+app.use(method('_method'))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.set('view engine', 'ejs');
 
 
