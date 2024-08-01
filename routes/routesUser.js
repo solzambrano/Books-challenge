c23ecdcc863f7efbc35795e91cd80fc6ab460eea
solzambrano <solzambrano.8@gmail.com>
2024-08-01 12:07:39 -0300

    fix validaciones formularios

--- routes/routesUser.js
+++ routes/routesUser.js
@@ -3,14 +3,16 @@ const router=express.Router();
 const errorMiddleware=require('../middleware/errorMiddleware');
 const validationUser=require('../middleware/validationUserLoginMiddleware')
 const handlerLogin=require('../middleware/validationCategoryMiddleware');
-const checkUser=require('../middleware/checkUserMiddleware')
+const checkUser=require('../middleware/checkUserMiddleware');
+const validationLogin=require('../middleware/validationLoginMiddleware');
+const validationRegister=require('../middleware/validationRegisterMiddleware');
 const userController=require('../controllers/userController')
 
 router.get('/login',userController.login)
 router.get('/register',userController.register)
 router.get("/logout", userController.logout);
-router.post('/login',validationUser,errorMiddleware('login'),handlerLogin,userController.processLogin)
-router.post('/register',checkUser,errorMiddleware('register'),userController.processRegister)
+router.post('/login',validationUser,validationLogin,errorMiddleware('login'),handlerLogin,userController.processLogin)
+router.post('/register',checkUser,validationRegister,errorMiddleware('register'),userController.processRegister)
 
 
 
