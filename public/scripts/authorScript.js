fa83a272d86ecec6185f7e4521cc28253f85649b
solzambrano <solzambrano.8@gmail.com>
2024-08-29 17:36:07 -0300

    boton opcional autor

--- public/scripts/authorScript.js
+++ public/scripts/authorScript.js
@@ -2,12 +2,24 @@ let addAuthor=document.getElementById("addAuthor");
 let formAuthor=document.getElementById("formAuthor");
 let cancelAuthor=document.getElementById("cancelAuthor");
 let cancel =document.getElementsByClassName("cancel");
+let selectAuthor= document.getElementById('selectAuthor')
+let link=document.getElementById('linkAuthor');
+
 
 document.addEventListener("DOMContentLoaded", (event) => {
+  selectAuthor.style.display='none'
   formAuthor.style.display='none';
   cancelAuthor.style.display='none';
 });
-
+link.addEventListener("click",(e)=>{
+    if(e.target.innerText=='link author'){
+        e.target.innerText="unlink author"
+        selectAuthor.style.display='flex'
+    }else{
+        e.target.innerText="link author"
+        selectAuthor.style.display='none'
+    }
+})
 addAuthor.addEventListener("click",()=>{
     formAuthor.style.display="block";
     cancelAuthor.style.display='block';
