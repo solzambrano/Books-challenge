bea77a2fd8e9176803e71733c629612840492838
solzambrano <solzambrano.8@gmail.com>
2024-08-30 17:03:46 -0300

    agregar selects a autor

--- public/scripts/scriptSelect.js
+++ public/scripts/scriptSelect.js
@@ -1,15 +1,22 @@
-let idSelect =document.getElementById('plus');
-let selectContainer =document.getElementById("select-container")
-console.log(selectContainer);
-
-idSelect.addEventListener('click',()=>{
+let selectContainer = document.getElementById("select-container");
+let addAuthor = document.getElementById('add');
+addAuthor.addEventListener('click',()=>{
      fetch('http://localhost:3000/authors/select')
        .then(res => res.text()) // Asegúrate de convertir la respuesta a texto
         .then(html => {
-            console.log('html',html)
             let tempDiv = document.createElement('div');
             tempDiv.innerHTML = html;
-            selectContainer.appendChild(tempDiv.firstElementChild);
-        }).catch(err=>console.log(err))
-
-})
+              let newSelect = tempDiv.firstElementChild;
+                
+                // Agregar un botón de eliminación individual a cada nuevo select
+                let deleteButton = document.createElement('button');
+                deleteButton.textContent = 'X';
+                deleteButton.className = 'delete-select col-md-1 btn btn-primary';
+                deleteButton.addEventListener('click', function() {
+                    selectContainer.removeChild(this.parentElement);
+                });
+                
+                newSelect.appendChild(deleteButton);
+                selectContainer.appendChild(newSelect);  
+        })
+  }).catch(err=>console.log(err))
