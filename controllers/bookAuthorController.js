0521812b36b2c35687c685c789279a1cdbd70313
solzambrano <solzambrano.8@gmail.com>
2024-09-09 23:02:42 -0300

    fix creacion de autor/libro

--- controllers/bookAuthorController.js
+++ controllers/bookAuthorController.js
@@ -2,8 +2,7 @@ const { ExclusionConstraintError } = require('sequelize');
 let db = require('../database/models');
 
 const bookAuthorController={
-    checkExists:async (model,field,value)=>{
-        console.log('parameters',model,field,value);  
+    checkExists:async (model,field,value)=>{ 
         return await model.findOne({
             where:{
                 [field]:value
@@ -11,14 +10,15 @@ const bookAuthorController={
         })
     },
     differentAuthor:(data)=>{
-        console.log('data',data);
          let author = Array.isArray(data.author) ? data.author : (data.author ? [data.author] : []);
-         data.nameAuthor!== 'Selected author' ? author.push(data.nameAuthor):author
+         data.nameAuthor!== 'Selected author' &&  data.nameAuthor!== undefined ? 
+         author.push(data.nameAuthor):author
          return author
     },
-    createModelAuthor:async(data)=>{
+    createModelAuthor:async(req,data)=>{  
        return await db.Author.create({
              name:data,
+             country:req.body.country??''
                 })
     },
     createModelBook:async(data)=>{
@@ -31,33 +31,37 @@ const bookAuthorController={
     processCreate: async(req,res)=>{   
         let authorModels=[];
         let bookModels=[];
-        let newAuthor=bookAuthorController.differentAuthor(req.body)//junto el autor de select y el del input
+       let newAuthor=bookAuthorController.differentAuthor(req.body)//junto el autor de select y el del input         
          if(newAuthor.length !==0){//si hay autores
             for(author of newAuthor){
                 let authorModel=await bookAuthorController.checkExists(db.Author,'Name',author);//verifico s cada autor existe
             if(!authorModel){
-                authorModel= await bookAuthorController.createModelAuthor(author)
+                authorModel= await bookAuthorController.createModelAuthor(req,author)
             }
             authorModels.push(authorModel)
-         }
+            }
          }    
          //esta parte es para la creacion de autores(1autor)
-        let books=Array.isArray(req.body.title)?req.body.title:[req.body.title];
-        for(book of books){
-            let bookModel= await bookAuthorController.checkExists(db.Book,'title',book)
-              if(!bookModel){
-                 bookModel=await bookAuthorController. createModelBook(req.body)  
-             }
-             bookModels.push(bookModel)
-         }// fin de for
-    if(authorModels.length!=0 && bookModels.length != 0){
+         if(req.body.title){
+            let books=Array.isArray(req.body.title)?req.body.title:[req.body.title];
+            if(books.length !==0){ 
+                for(book of books){
+                    let bookModel= await bookAuthorController.checkExists(db.Book,'title',book)
+                    if(!bookModel){
+                        bookModel=await bookAuthorController. createModelBook(req.body)  
+                    }
+                bookModels.push(bookModel)
+                }// fin de for
+            }
+         }
+        if(authorModels.length!=0 && bookModels.length != 0){
         for(authorModel of authorModels){
             for(bookModel of bookModels){
                 await authorModel.addBook(bookModel)
                 await bookModel.addAuthor(authorModel)
             }
         }
-    }
+        }
     res.redirect('/books');
     }
 
